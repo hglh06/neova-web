b5b4b4a5e8143d554ab84b0e5eab908584a30ef1
Hector Lopez <hglh06@gmail.com>
2026-04-26 13:37:28 -0600

    update

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -1,7 +1,7 @@
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/5216123489328?text=Hola%20quiero%20información%20sobre%20rafia%20agrícola"
+      href="https://wa.me/5213328289691?text=Hola%20quiero%20información%20sobre%20rafia%20agrícola"
       target="_blank"
       className="fixed bottom-6 right-6 bg-green-500 text-white px-4 py-3 rounded-full shadow-lg"
     >
